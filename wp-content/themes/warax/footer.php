95894241fa4c41ec27fb011637aff0a4c078cb2d
Vio Nikolov <vi0nikk@gmail.com>
2018-01-22 00:54:03 +0200

    Setting nav menus locations in the header and in the footer, added  in functions register_nav_menu

--- wp-content/themes/warax/footer.php
+++ wp-content/themes/warax/footer.php
@@ -1,6 +1,6 @@
 
     <footer>
-        <p>my footer</p>
+        <?php wp_nav_menu(array('theme_location' => 'secondary')); ?>
     </footer>
 
     <?php wp_footer(); ?>
--- wp-content/themes/warax/functions.php
+++ wp-content/themes/warax/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-
+//adding scripts and styles to wp
 function warax_script_enqueue (){
     wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/warax.css', array(), '1.0.0', 'all');
     wp_enqueue_script('customjs', get_template_directory_uri() . '/js/warax.js', array(), '1.0.0', true);
@@ -8,8 +8,14 @@ function warax_script_enqueue (){
 
 add_action('wp_enqueue_scripts', 'warax_script_enqueue');
 
+
+
+//activating menus in wp
 function warax_theme_setup () {
     add_theme_support('menus');
+
+    register_nav_menu('primary', 'Primary header navigation');
+    register_nav_menu('secondary', 'Footer menu navigation');
 }
 
 add_action('init', 'warax_theme_setup');
